6dd18fa20219cd999be6373921dbdc718a4cfc69
pranay292002 <76903075+pranay292002@users.noreply.github.com>
2021-01-14 19:18:04 +0530

    Add files via upload
    this is my 2nd website

---
+++ script.js
@@ -0,0 +1,62 @@
+function avenger() {
+             
+             document.querySelector(".js").innerHTML= "Iron Man is a fictional superhero who wears a suit of armor.<br> His alter ego is Tony Stark. <br>He was created by Stan Lee, Jack Kirby and Larry Lieber for Marvel Comics in Tales of Suspense <br> in the year 1963 and appears in their comic books <br>and the tritagonist in the Marvel Universe.";
+}
+
+
+function iron () {
+    document.getElementById("info").innerHTML="  <p> Iron Man is a fictional superhero who wears a suit of armor.His alter ego is Tony Stark. <br>He was created by Stan Lee, Jack Kirby and Larry Lieber for Marvel Comics in Tales of Suspense  in the year 1963 and appears in their comic books and the tritagonist in the Marvel Universe.</p>";
+
+}
+function cap () {
+    document.getElementById("info").innerHTML="<p>Captain America is the alter ego of Steve Rogers, a frail young man enhanced to the peak of human perfection by an experimental serum to aid the United States government's efforts in World War II. Near the end of the war, he was trapped in ice and survived in suspended animation until he was revived in modern times.  </p>";
+
+}
+function spider () {
+    document.getElementById("info").innerHTML="  <p>Spider-Man is a fictional superhero in the Marvel Comics and the main protagonist in the Marvel Universe. He is Marvel's mascot. He is a character that was created by writer Stan  Lee and artist Steve Ditko. He first appeared in Amazing Fantasy #15 in August 1962.</p>";
+
+}
+function thor () {
+    document.getElementById("info").innerHTML="  <p>Thor OdinsonThor The son of Odin uses his mighty abilities as the God of Thunder to protect his home Asgard and planet Earth alike. Thor Odinson wields the power of the ancient Asgardians to fight evil throughout the Nine Realms and beyond.</p>";
+
+}
+function widow () {
+    document.getElementById("info").innerHTML="  <p>The first and best-known Black Widow is a Russian agent trained as a spy, martial artist, and sniper, and outfitted with an arsenal of high-tech weaponry, including a pair of wrist-mounted energy weapons dubbed her Widow's Bite. She wears no costume during her first few appearances but simply evening wear and a veil.</p>";
+
+}
+function vision () {
+    document.getElementById("info").innerHTML="  <p>Vision is a fictional superhero appearing in American comic books published by Marvel Comics. He is an android and a member of the Avengers who first appeared in The Avengers #57 (October 1968). He is loosely based on the Timely Comics character of the same name.</p>";
+
+}
+function hulk () {
+    document.getElementById("info").innerHTML="  <p> In his comic book appearances, the character is both the Hulk, a green-skinned, hulking and muscular humanoid possessing a vast degree of physical strength, and his alter ego Dr. Robert Bruce Banner, a physically weak, socially withdrawn, and emotionally reserved physicist.</p>";
+
+}
+function wanda() {
+    document.getElementById("info").innerHTML="  <p>The Scarlet Witch possesses abilities to alter reality in unspecified ways and is a powerful sorceress. The Scarlet Witch is later depicted as a regular member of the Avengers superhero team. She also becomes the wife of fellow superhero and teammate Vision, with whom she has two sons, Thomas and William.</p>";
+
+}
+function hawkeye () {
+    document.getElementById("info").innerHTML="  <p>Hawkeye (Clinton Francis Barton) is a fictional superhero appearing in American comic books ... In 2003, Hawkeye had a short-lived ongoing series, Hawkeye (vol. ... When the authorities arrived, Black Widow left the Red Room where she left a note for Hawkeye to stop following her and for Winter Soldier to join her  </p>";
+
+}
+function falcon () {
+    document.getElementById("info").innerHTML="  <p>As the superhero Falcon, Wilson uses mechanical wings to fly, and has limited telepathic and empathic control over birds. Following Steve Rogers's retirement, Wilson becomes the newest Captain America in All-New Captain America #1 (January 2015) and leader of the Avengers. </p>";
+
+}
+function winter () {
+    document.getElementById("info").innerHTML="  <p> Having trained under Steve Rogers (the original Captain America in World War II) and others in the time leading up to World War II, Bucky Barnes is a master of hand-to-hand combat and martial arts, as well as being skilled in the use of military weapons such as firearms and grenades.</p>";
+
+}
+function panther () {
+    document.getElementById("info").innerHTML="  <p> Thousands of years ago, five African tribes war over a meteorite containing the metal vibranium. One warrior ingests a heart-shaped herb affected by the metal and gains superhuman abilities, becoming the first Black Panther.</p>";
+
+}
+function doctor () {
+    document.getElementById("info").innerHTML="  <p>Doctor Strange serves as the Sorcerer Supreme, the primary protector of Earth against magical and mystical threats. Inspired by stories of black magic and Chandu the Magician, Strange was created during the Silver Age of Comic Books to bring a different kind of character and themes of mysticism to Marvel Comics.</p>";
+
+}
+function ant () {
+    document.getElementById("info").innerHTML="  <p>Ant-Man is the name of several superheroes appearing in books published by Marvel Comics. ... He had his own short-lived title before being part of other teams such as joining Avengers:</p>";
+
+}
